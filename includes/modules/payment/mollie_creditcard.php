0797cc5e2976935b89d3eaa3d5d63b575709d2ba
ivan_bojovic <ivan.bojovic@logeecom.com>
2020-11-23 13:04:42 +0100

    Add additional configuration for credit card method
    ISSUE: MOCRSET04-6

--- includes/modules/payment/mollie_creditcard.php
+++ includes/modules/payment/mollie_creditcard.php
@@ -9,4 +9,14 @@ class mollie_creditcard extends mollie
 {
     public $title = 'Credit card';
 
+    public function _configuration()
+    {
+        $config = parent::_configuration();
+        $config['COMPONENTS_STATUS'] = [
+            'configuration_value' => 'True',
+            'set_function'        => 'gm_cfg_select_option(array(\'True\', \'False\'), ',
+        ];
+
+        return $config;
+    }
 }
