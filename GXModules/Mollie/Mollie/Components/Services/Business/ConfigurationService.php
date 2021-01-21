33ee0e6e78a3eae225ba8240dafe470fd4bf1b35
predrag.krstojevic <predrag.krstojevic@logeecom.com>
2021-01-21 15:34:15 +0100

    Update version 2.0.5

--- GXModules/Mollie/Mollie/Components/Services/Business/ConfigurationService.php
+++ GXModules/Mollie/Mollie/Components/Services/Business/ConfigurationService.php
@@ -12,7 +12,7 @@ use Mollie\Gambio\Utility\UrlProvider;
  */
 class ConfigurationService extends Configuration
 {
-    const MOLLIE_GAMBIO_VERSION = '2.0.4';
+    const MOLLIE_GAMBIO_VERSION = '2.0.5';
 
     /**
      * @inheritDoc
