63b86851834aa4ecac58b41d6b37429f12b68876
ivan_bojovic <ivan.bojovic@logeecom.com>
2021-02-10 09:06:44 +0100

    Extend payment method configuration with transaction description
    ISSUE: MOCRSET05-41

--- lang/english/modules/payment/mollie_klarnapaylater.php
+++ lang/english/modules/payment/mollie_klarnapaylater.php
@@ -29,3 +29,6 @@ define('MODULE_PAYMENT_MOLLIE_KLARNAPAYLATER_SORT_ORDER_DESC', 'Lowest is displa
 
 define('MODULE_PAYMENT_MOLLIE_KLARNAPAYLATER_ORDER_EXPIRES_TITLE', 'Days To Expire');
 define('MODULE_PAYMENT_MOLLIE_KLARNAPAYLATER_ORDER_EXPIRES_DESC', 'How many days before orders for this method becomes expired? Leave empty to use default expiration (28 days)<br><br>Please note: It is not possible to use an expiry date more than 28 days in the future, unless another maximum is agreed between the merchant and Klarna.');
+
+define('MODULE_PAYMENT_MOLLIE_KLARNAPAYLATER_TRANSACTION_DESCRIPTION_TITLE', 'Transaction description');
+define('MODULE_PAYMENT_MOLLIE_KLARNAPAYLATER_TRANSACTION_DESCRIPTION_DESC', 'The description to be used for payment transaction. These variables are available: {orderNumber}, {storeName}, {customerFirstname}, {customerLastname}, {customerCompany}, and {cartNumber}.');
--- lang/english/modules/payment/mollie_paypal.php
+++ lang/english/modules/payment/mollie_paypal.php
@@ -29,3 +29,6 @@ define('MODULE_PAYMENT_MOLLIE_PAYPAL_SORT_ORDER_DESC', 'Lowest is displayed firs
 
 define('MODULE_PAYMENT_MOLLIE_PAYPAL_ORDER_EXPIRES_TITLE', 'Days To Expire');
 define('MODULE_PAYMENT_MOLLIE_PAYPAL_ORDER_EXPIRES_DESC', 'How many days before orders for this method becomes expired? Leave empty to use default expiration (28 days)');
+
+define('MODULE_PAYMENT_MOLLIE_PAYPAL_TRANSACTION_DESCRIPTION_TITLE', 'Transaction description');
+define('MODULE_PAYMENT_MOLLIE_PAYPAL_TRANSACTION_DESCRIPTION_DESC', 'The description to be used for payment transaction. These variables are available: {orderNumber}, {storeName}, {customerFirstname}, {customerLastname}, {customerCompany}, and {cartNumber}.');
