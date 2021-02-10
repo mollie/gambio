bbf9461c11b97dedc4558995553265bdf26657c8
ivan_bojovic <ivan.bojovic@logeecom.com>
2021-02-10 13:57:29 +0100

    Do not preselect first issuer in the list
    Adjust to GX3
    
    ISSUE: CS-1700

--- templates/Honeygrid/usermod/javascript/Checkout/mollie-issuers.js
+++ templates/Honeygrid/usermod/javascript/Checkout/mollie-issuers.js
@@ -7,6 +7,16 @@
         showIssuerList();
         showIssuersIfOnlyPaymentMethod();
 
+        let checkoutForm = document.querySelector('#checkout_payment');
+        checkoutForm.addEventListener('submit', async event => {
+            if (!isIssuersSelected()) {
+                showIssuerErrorMessage();
+                event.preventDefault();
+                event.stopPropagation();
+            }
+        });
+
+
         for (let i = 0; i < issuerListWrappers.length; i++) {
             addIssuerListListeners(issuerListWrappers[i]);
         }
@@ -84,5 +94,47 @@
                 issuerListWrapper.classList.remove('mollie-hidden');
             }
         }
+
+        /**
+         * Check if issuer is selected (if the payment method with the issuer's list is active)
+         *
+         * @returns {boolean}
+         */
+        function isIssuersSelected() {
+            let issuerListWrapper = getActiveIssuerListWrapper();
+            if (issuerListWrapper) {
+                return issuerListWrapper.querySelectorAll('input[type="radio"]:checked').length > 0;
+            }
+
+            return true;
+        }
+
+        /**
+         * Displays error message in the issuer list box
+         */
+        function showIssuerErrorMessage() {
+            let issuerListWrapper = getActiveIssuerListWrapper();
+            if (issuerListWrapper) {
+                let issuerErrorMessage = issuerListWrapper.querySelector('.issuer-not-selected');
+                if (issuerErrorMessage) {
+                    issuerErrorMessage.classList.remove('hidden')
+                }
+            }
+        }
+
+        /**
+         * Returns issuer list wrapper of the selected payment method
+         *
+         * @returns {null|Element}
+         */
+        function getActiveIssuerListWrapper() {
+            let activeMethodWrapper = document.querySelector('.list-group-item.active');
+            if (activeMethodWrapper) {
+                return  activeMethodWrapper.querySelector('.mollie-issuer-list-wrapper');
+            }
+
+            return null;
+        }
+
     });
 })();
