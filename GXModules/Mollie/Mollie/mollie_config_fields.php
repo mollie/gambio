81a6a2f32d0fe477d570c3bd7bb5f0dd58ebdfe5
ivan_bojovic <ivan.bojovic@logeecom.com>
2021-03-08 02:06:27 +0100

    Add payment method update handler
    Adjust to GX4.0 - GX3.5
    
    ISSUE: MOCRSET05-71

--- GXModules/Mollie/Mollie/mollie_config_fields.php
+++ GXModules/Mollie/Mollie/mollie_config_fields.php
@@ -96,21 +96,6 @@ function mollie_multi_language_text($key_value, $key = '')
 
     return mollie_multi_language_field($templatePath, $key_value, $key);
 }
-/**
- * Renders multi lang input
- *
- * @param string $key_value
- * @param string $key
- *
- * @return string|string[]
- * @throws Exception
- */
-function mollie_multi_language_text_area($key_value, $key = '')
-{
-    $templatePath = PathProvider::getAdminTemplatePath('multi_lang_text_area.html', 'ConfigFields');
-
-    return mollie_multi_language_field($templatePath, $key_value, $key);
-}
 
 /**
  * @param string $templatePath
