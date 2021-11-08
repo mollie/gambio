8dfea7c4d3131189d5709b935058ceaa0e446bc2
predrag.krstojevic <predrag.krstojevic@logeecom.com>
2021-11-08 11:55:58 +0100

    Update CORE to version 1.3.3

---
+++ system/classes/external/mollie/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '0.1.10',
+        'version' => '0.1.10.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'mollie/gambio',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mollie/gambio' => array(
+            'pretty_version' => '0.1.10',
+            'version' => '0.1.10.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'mollie/integration-core' => array(
+            'pretty_version' => '1.3.3',
+            'version' => '1.3.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mollie/integration-core',
+            'aliases' => array(),
+            'reference' => 'a60a6e1cffb055dc83d6829ae15f199e177199b4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
--- system/classes/external/mollie/vendor/mollie/integration-core/CHANGELOG.md
+++ system/classes/external/mollie/vendor/mollie/integration-core/CHANGELOG.md
@@ -3,7 +3,7 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/).
 
-## Unreleased
+## [v1.3.3](https://github.com/mollie/orocore/tree/v1.3.3) - 2021-11-08
 - Added support for the Klarna Pay Now payment method.
 
 ## [v1.3.2](https://github.com/mollie/orocore/tree/v1.3.2) - 2021-10-07
