7b3493396a3e0e0b1837bf99cf7f07504f3b994a
Tamara <tamara@example.com>
2022-03-04 17:51:01 +0100

    Fix - Missing labels on the Mollie Payment Surcharge
    MOCRSET10-36

--- lang/english/modules/order_total/mollie.php
+++ lang/english/modules/order_total/mollie.php
@@ -3,3 +3,10 @@
 define('MODULE_ORDER_TOTAL_MOLLIE_HEADER', 'Mollie Payment Surcharge');
 define('MODULE_ORDER_TOTAL_MOLLIE_TITLE', 'Mollie Payment Surcharge');
 define('MODULE_ORDER_TOTAL_MOLLIE_DESCRIPTION', 'Calculation of the Mollie payment methods');
+define('MODULE_ORDER_TOTAL_MOLLIE_STATUS_TITLE', 'Display Mollie Payment Surcharge');
+define('MODULE_ORDER_TOTAL_MOLLIE_STATUS_DESC', 'Enable Mollie surcharge');
+define('MODULE_ORDER_TOTAL_MOLLIE_TAX_CLASS_TITLE', 'Tax Class');
+define('MODULE_ORDER_TOTAL_MOLLIE_TAX_CLASS_DESC', 'Select a tax class');
+define('MODULE_ORDER_TOTAL_MOLLIE_SORT_ORDER_TITLE', 'Sort Order');
+define('MODULE_ORDER_TOTAL_MOLLIE_SORT_ORDER_DESC', 'Display sort order');
+
