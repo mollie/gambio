4539c975caceb797e8a645947e9e00722a26a2ab
Tamara <tamara@example.com>
2022-03-09 12:53:51 +0100

    Fix - No default values for single-click labels
    Note: Use the subtotal for the order instead of the total to calculate the surcharge
    
    MOCRSET10-33

--- GXModules/Mollie/Mollie/Components/Update/v2_0_8/PaymentMethodUpdate.php
+++ GXModules/Mollie/Mollie/Components/Update/v2_0_8/PaymentMethodUpdate.php
@@ -2,8 +2,11 @@
 
 namespace Mollie\Gambio\Update\v2_0_8;
 
+use LanguageTextManager;
+use Mollie\BusinessLogic\Surcharge\SurchargeType;
 use Mollie\Gambio\Entity\Repository\GambioConfigRepository;
 use Mollie\Gambio\Entity\Repository\GambioLanguageRepository;
+use StaticGXCoreLoader;
 
 /**
  * Class PaymentMethodUpdate
@@ -111,6 +114,26 @@ class PaymentMethodUpdate
                 'key' => 'TRANSACTION_DESCRIPTION',
                 'value' => static::TRANSACTION_DESC_DEFAULT_VALUE,
                 'set_function' => 'mollie_multi_language_text( ',
+            ],
+            [
+                'key' => 'SURCHARGE_TYPE',
+                'value' => SurchargeType::NO_FEE,
+                'set_function' => 'mollie_surcharge_type_select( ',
+            ],
+            [
+                'key' => 'SURCHARGE_FIXED_AMOUNT',
+                'value' => 0,
+                'set_function' => 'mollie_input_number( ',
+            ],
+            [
+                'key' => 'SURCHARGE_PERCENTAGE',
+                'value' => 0,
+                'set_function' => 'mollie_input_number( ',
+            ],
+            [
+                'key' => 'SURCHARGE_LIMIT',
+                'value' => 0,
+                'set_function' => 'mollie_input_number( ',
             ]
         ];
 
@@ -123,6 +146,78 @@ class PaymentMethodUpdate
             ];
         }
 
+        if($this->code === 'mollie_creditcard')
+        {
+            $configFields[] = [
+                'key' => 'SINGLE_CLICK_APPROVAL_TEXT',
+                'value' => $this->translate($_SESSION['language_code'], 'mollie_single_click_payment_approval_text'),
+                'set_function' => 'mollie_multi_language_text( ',
+            ];
+
+            $configFields[] = [
+                'key' => 'SINGLE_CLICK_DESCRIPTION',
+                'value' => $this->translate($_SESSION['language_code'], 'mollie_single_click_payment_desc'),
+                'set_function' => 'mollie_multi_language_text( ',
+            ];
+
+            foreach ($languageRepository->getLanguages() as $language) {
+                $code = strtoupper($language['code']);
+                $configFields[] = [
+                    'key' => 'SINGLE_CLICK_APPROVAL_TEXT_' . $code,
+                    'value' => $this->translate($code, 'mollie_single_click_payment_approval_text'),
+                ];
+                $configFields[] = [
+                    'key' => 'SINGLE_CLICK_DESCRIPTION_' . $code,
+                    'value' => $this->translate($code, 'mollie_single_click_payment_desc'),
+                ];
+            }
+        }
+
         return $configFields;
     }
+
+    /**
+     * Retruns translated phrase
+     *
+     * @param string $langCode
+     * @param string $phrase
+     *
+     * @return string
+     */
+    protected function translate($langCode, $phrase)
+    {
+        return LanguageTextManager::get_instance('module_center_module', $this->_getLanguageId($langCode))->get_text($phrase);
+    }
+
+    /**
+     * Returns language id based on lang code
+     *
+     * @param string $langCode
+     *
+     * @return int
+     */
+    protected function _getLanguageId($langCode)
+    {
+        $languages = $this->_getLanguages();
+        foreach ($languages as $language) {
+            if ($language['code'] === strtolower($langCode)) {
+                return $language['id'];
+            }
+        }
+
+        // default language as fallback
+        return 0;
+    }
+
+    protected function _getLanguages()
+    {
+        $db = StaticGXCoreLoader::getDatabaseQueryBuilder();
+        $languages = $db->select('*')
+            ->select('languages_id AS id')
+            ->order_by('sort_order ASC')
+            ->get('languages')
+            ->result_array();
+        return $languages;
+    }
 }
+
