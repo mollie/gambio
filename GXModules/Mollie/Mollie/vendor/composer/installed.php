59ab8d79cdf127c0dfc1d6384ef9d4ee8c156e4a
Luka Simovic <luka.simovic@logeecom.com>
2024-04-19 11:34:51 +0200

    Release version 3.0.22
    ISSUE: PIGAM-174, PIGAM-175

--- GXModules/Mollie/Mollie/vendor/composer/installed.php
+++ GXModules/Mollie/Mollie/vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '3.0.21',
-        'version' => '3.0.21.0',
+        'pretty_version' => '3.0.22',
+        'version' => '3.0.22.0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'mollie/gambio' => array(
-            'pretty_version' => '3.0.21',
-            'version' => '3.0.21.0',
+            'pretty_version' => '3.0.22',
+            'version' => '3.0.22.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
