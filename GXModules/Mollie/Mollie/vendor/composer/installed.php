cc9289b7780c98b6d4312d0ba3a7ecce2ab5a56f
Luka Simovic <luka.simovic@logeecom.com>
2026-04-07 16:18:24 +0200

    Release 2.1.1

--- GXModules/Mollie/Mollie/vendor/composer/installed.php
+++ GXModules/Mollie/Mollie/vendor/composer/installed.php
@@ -1,7 +1,7 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '2.1.0',
-        'version' => '2.1.0.0',
+        'pretty_version' => '2.1.1',
+        'version' => '2.1.1.0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'mollie/gambio' => array(
-            'pretty_version' => '2.1.0',
-            'version' => '2.1.0.0',
+            'pretty_version' => '2.1.1',
+            'version' => '2.1.1.0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
--- README.md
+++ README.md
@@ -41,6 +41,9 @@ Read more about the integration configuration on [our Wiki](https://github.com/m
 
 # Release notes
 
+*2.1.1*
+- Resolve net price calculation logic
+
 *2.1.0*
 - Add Capture functionality to the plugin
 - Remove usage of Orders API
